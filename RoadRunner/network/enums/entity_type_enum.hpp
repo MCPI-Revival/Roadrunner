6dcbe1672058508d99686e9b0dbb5dbe5eedc019
MFDGaming <alexandros.argentakis@gmail.com>
2023-07-08 21:01:10 +0300

    added EntityTypeEnum

---
+++ RoadRunner/network/enums/entity_type_enum.hpp
@@ -0,0 +1,24 @@
+namespace RoadRunner {
+    namespace network {
+        namespace enums {
+            enum class EntityTypeEnum {
+                chicken = 10,
+                cow = 11,
+                pig = 12,
+                sheep = 13,
+                zombie = 32,
+                creeper = 33,
+                skeleton = 34,
+                spider = 35,
+                pig_zombie = 36,
+                item_entity = 64,
+                primed_tnt = 65,
+                falling_tile = 66,
+                arrow = 80,
+                snowball = 81,
+                thrown_egg = 82,
+                painting = 83
+            };
+        }
+    }
+}
