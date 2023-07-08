1b388218bc88e5520b950f0dfe073e42daa5f2eb
MFDGaming <alexandros.argentakis@gmail.com>
2023-07-08 12:13:29 +0300

    added enums folder

---
+++ RoadRunner/network/enums/level_event_id_enum.hpp
@@ -0,0 +1,10 @@
+namespace RoadRunner {
+    namespace network {
+        namespace enums {
+            enum class LevelEventIdEnum {
+                random_door_sound = 1003,
+                all_players_sleep = 9800
+            };
+        }
+    }
+}
---
+++ RoadRunner/network/enums/login_status_enum.hpp
@@ -0,0 +1,11 @@
+namespace RoadRunner {
+    namespace network {
+        namespace enums {
+            enum class LoginStatusEnum {
+                success = 0,
+                outdated_client = 1,
+                outdated_server = 2
+            };
+        }
+    }
+}
